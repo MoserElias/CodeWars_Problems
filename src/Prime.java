8014775352c80401d1d959b234ae70028d1ecf0a
Elias Moser <elias.moser@aol.com>
2022-07-26 18:09:13 +0200

    Prime

---
+++ src/Prime.java
@@ -0,0 +1,27 @@
+public class Prime {
+    public static boolean isPrime(int num) {
+        //edge cases
+        int n = num;
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(10));
+    }
+}
