e4419bf6f9ff2035268f9458e3527d9552363613
Elias Moser <elias.moser@aol.com>
2022-08-10 14:58:53 +0200

    RowSumOddNumbers

---
+++ src/RowSumOddNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+        return n * n * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rowSumOddNumbers(4));
+    }
+
+}
+
