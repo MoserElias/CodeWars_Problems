a3865198259337b13bbf23c406cc016c7ad05ba3
Elias Moser <elias.moser@aol.com>
2022-08-14 19:08:50 +0200

    AscendDescend

--- src/AscendDescend.java
+++ src/AscendDescend.java
@@ -1,28 +1,19 @@
 public class AscendDescend {
 
     public static String ascendDescend(int length, int minimum, int maximum) {
-        //write string with integers with length from minimum to maximum
-        StringBuilder result = new StringBuilder();
-
-        final int orgMinimum = minimum;
-        final int orgMaxmimum = maximum;
-
-        for (int i = 0; i < length; i++) {
-            if (minimum == maximum) {
-                maximum = orgMaxmimum;
-                minimum = orgMinimum;
-                result.append(minimum);
-                minimum++;
-            } else if (minimum < maximum) {
-                result.append(minimum);
-                minimum++;
-            } else {
-                result.append(maximum);
-                maximum--;
-            }
+        String result = "";
+        for (int i = minimum; i < maximum; i++) {
+            result = result + i;
+        }
+        for (int i = maximum; i > minimum; i--) {
+            result = result + i;
         }
 
-        return result.toString();
+        if (result.length() == 0) {
+            return result;
+        }
+        result = result.repeat(length / result.length() + 1);
+        return result.substring(0, length);
     }
 
 
