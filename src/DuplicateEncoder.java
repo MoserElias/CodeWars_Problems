ef8c8d95e227ed160eb21da012db037fde8e7e7e
Elias Moser <elias.moser@aol.com>
2022-08-16 07:23:33 +0200

    DuplicateEncoder

---
+++ src/DuplicateEncoder.java
@@ -0,0 +1,27 @@
+public class DuplicateEncoder {
+
+    public static String encode(String word) {
+        StringBuilder result = new StringBuilder();
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            int times = 0;
+            char charToCheck = word.charAt(i);
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) == charToCheck && i != j) {
+                    times++;
+                }
+            }
+            if (times == 0) {
+                result.append("(");
+            } else {
+                result.append(")");
+            }
+        }
+        return result.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(encode("(( @"));
+    }
+}
