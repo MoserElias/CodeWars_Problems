065c46050a0067a017573671236699d11fcb6033
Elias Moser <elias.moser@aol.com>
2022-08-28 12:59:27 +0200

    Zeros

---
+++ src/Zeros.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class Zeros {
+
+    public static int zeros(int n) {
+        if (n < 0) {
+            return -1;
+        }
+
+        int count = 0;
+        for (int i = 5; n / i >= 1; i *= 5) {
+            count += n / i;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(zeros(100));
+    }
+}
