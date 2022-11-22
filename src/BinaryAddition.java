aa5d7784d17b8a0cb076b574328ee6ce044cdcf2
Elias <elias.moser@aol.com>
2022-11-22 10:30:34 +0100

    BinaryAddition

---
+++ src/BinaryAddition.java
@@ -0,0 +1,13 @@
+public class BinaryAddition {
+
+    public static String binaryAddition(int a, int b) {
+        int sum = a + b;
+        String result = Integer.toBinaryString(sum);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binaryAddition(1, 1));
+    }
+}
+
