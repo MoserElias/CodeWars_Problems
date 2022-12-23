ea880c28bc27c3c794ced093818a0796a472f4ad
Elias <elias.moser@aol.com>
2022-12-23 13:29:39 +0100

    Edit gitignore

---
+++ src/TwoToOne.java
@@ -0,0 +1,22 @@
+public class TwoToOne {
+    public static String longest(String s1, String s2) {
+	String result = "";
+
+	for (int index = 0; index < s1.length(); index++) {
+	    char toInsert = s1.charAt(index);
+	    for(int index2 = 0; index2 < s2.length(); index2++) {
+		if(index != index2) {
+		    
+		}
+	    }
+	    
+	}
+	
+	
+	return result; 
+    }
+    
+    public static void main(String[] args) {
+	
+    }
+}
