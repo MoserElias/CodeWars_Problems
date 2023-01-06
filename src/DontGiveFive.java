27e33206a47170b8aa70f5a81b656222942cce4c
Elias <elias.moser@aol.com>
2023-01-06 18:01:54 +0100

    Setup DontGiveFive

---
+++ src/DontGiveFive.java
@@ -0,0 +1,11 @@
+public class DontGiveFive {
+    public static int dontGiveMeFive(int start, int end) {
+        int length = 0;
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dontGiveMeFive(1, 9));
+    }
+}
