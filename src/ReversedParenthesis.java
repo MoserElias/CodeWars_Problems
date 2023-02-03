e9c30ace8f62c0587316d3bf10e555eb17cc5c97
Elias Moser <elias.moser@aol.com>
2023-02-03 17:42:58 +0100

    Working Solution

--- src/ReversedParenthesis.java
+++ src/ReversedParenthesis.java
@@ -1,37 +1,23 @@
 public class ReversedParenthesis {
 
     public static int solve(String s) {
-        //make balanced parenthesis
-        int initialLength = s.length();
-        int counter;
-        if (s.length() <= 2) {
-            counter = 0;
-            if (s.charAt(0) == '(' && s.charAt(s.length() - 1) != ')') {
-                counter += 1;
-            } else if (s.charAt(0) != '(' && s.charAt(s.length() - 1) != ')') {
-                counter += 2;
-            } else if (s.charAt(0) != '(' && s.charAt(s.length() - 1) == ')') {
-                counter += 1;
-            }
-        } else {
-            counter = 0;
-            while (s.length() > 1) {
-                //System.out.println(s.charAt(0) + " " + s.charAt(s.length() - 1));
-                if (s.charAt(0) == '(' && s.charAt(s.length() - 1) != ')') {
-                    counter += 1;
-                } else if (s.charAt(0) != '(' && s.charAt(s.length() - 1) != ')') {
-                    counter += 2;
-                } else if (s.charAt(0) != '(' && s.charAt(s.length() - 1) == ')') {
-                    counter += 1;
-                }
-                s = s.substring(1, s.length() - 1);
-            }
-        }
-        //System.out.println(counter);
+        if (s.length() % 2 != 0) return -1;
 
-        if (counter == 1 && initialLength <= 3) {
-            return -1;
+        //remove valid parenthesis
+        while (s.contains("()")) {
+            s = s.replace("()", "");
         }
+        System.out.println(s); //now only contains invalid parethesis
+        int counter = 0;
+
+        for (int i = 1; i < s.length(); i += 2) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                counter++;
+            } else {
+                counter += 2;
+            }
+        }
+
 
         return counter;
     }
