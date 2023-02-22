4bd21f7a057d133f5b2f6bfa451daabc87e4fce6
Elias Moser <elias.moser@aol.com>
2023-02-22 10:47:34 +0100

    GGT Algorithmus einfach

--- src/GCD.java
+++ src/GCD.java
@@ -1,42 +1,18 @@
 public class GCD {
 
     public static int compute(int x, int y) {
-        //search common divisor with mod computations
-        int xDivisor = 1;
-        int yDivisor = 1;
-
-
-
-
-        if (x < y) {
-            //search divisor for x
-            for (int i = 1; i <= x; i++) {
-                if (x % i == 0) {
-                    xDivisor = i;
-                }
+        //search common divisor with ggt algorithm
+        while (y != 0) {
+            if (x > y) {
+                x = x - y;
+            } else {
+                y = y - x;
             }
-
-            if (y % xDivisor == 0) {
-                return xDivisor;
-            }
-        } else {
-            //search divisor for y
-            for (int i = 1; i <= y; i++) {
-                if (y % i == 0) {
-                    yDivisor = i;
-                }
-            }
-
-            if (x % yDivisor == 0) {
-                return yDivisor;
-            }
-
         }
-        //if no common divisor is found
-        return 1;
+        return x;
     }
 
     public static void main(String[] args) {
-        System.out.println(compute(2672, 5678));
+        System.out.println(compute(30, 12));
     }
 }
